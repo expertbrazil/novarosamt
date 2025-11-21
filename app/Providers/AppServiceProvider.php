cbee15fbab5a2740868407f8105c270281a5761a
Jones <expertbrazilweb@gmail.com>
2025-11-21 08:44:57 -0300

    parametros

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,42 +32,46 @@ class AppServiceProvider extends ServiceProvider
         Carbon::setLocale('pt_BR');
         date_default_timezone_set($timezone);
         
-        // Ajusta SMTP com base nos parâmetros em produção
-        if (app()->environment('production') && Schema::hasTable('settings')) {
-            $mailConfig = [];
+        // Ajusta SMTP com base nos parâmetros do admin
+        if (Schema::hasTable('settings')) {
+            try {
+                $mailConfig = [];
 
-            $smtpHost = Settings::get('smtp_host');
-            $smtpPort = Settings::get('smtp_port');
-            $smtpUsername = Settings::get('smtp_username');
-            $smtpPassword = Settings::get('smtp_password');
-            $smtpEncryption = Settings::get('smtp_encryption');
-            $smtpFromAddress = Settings::get('smtp_from_address');
-            $smtpFromName = Settings::get('smtp_from_name');
+                $smtpHost = Settings::get('smtp_host');
+                $smtpPort = Settings::get('smtp_port');
+                $smtpUsername = Settings::get('smtp_username');
+                $smtpPassword = Settings::get('smtp_password');
+                $smtpEncryption = Settings::get('smtp_encryption');
+                $smtpFromAddress = Settings::get('smtp_from_address');
+                $smtpFromName = Settings::get('smtp_from_name');
 
-            if (!empty($smtpHost)) {
-                $mailConfig['mail.mailers.smtp.host'] = $smtpHost;
-            }
-            if (!empty($smtpPort)) {
-                $mailConfig['mail.mailers.smtp.port'] = (int) $smtpPort;
-            }
-            if (!empty($smtpUsername)) {
-                $mailConfig['mail.mailers.smtp.username'] = $smtpUsername;
-            }
-            if (!empty($smtpPassword)) {
-                $mailConfig['mail.mailers.smtp.password'] = $smtpPassword;
-            }
-            if (!empty($smtpEncryption)) {
-                $mailConfig['mail.mailers.smtp.encryption'] = $smtpEncryption;
-            }
-            if (!empty($smtpFromAddress)) {
-                $mailConfig['mail.from.address'] = $smtpFromAddress;
-            }
-            if (!empty($smtpFromName)) {
-                $mailConfig['mail.from.name'] = $smtpFromName;
-            }
-            if (!empty($mailConfig)) {
-                $mailConfig['mail.default'] = 'smtp';
-                Config::set($mailConfig);
+                if (!empty($smtpHost)) {
+                    $mailConfig['mail.mailers.smtp.host'] = $smtpHost;
+                }
+                if (!empty($smtpPort)) {
+                    $mailConfig['mail.mailers.smtp.port'] = (int) $smtpPort;
+                }
+                if (!empty($smtpUsername)) {
+                    $mailConfig['mail.mailers.smtp.username'] = $smtpUsername;
+                }
+                if (!empty($smtpPassword)) {
+                    $mailConfig['mail.mailers.smtp.password'] = $smtpPassword;
+                }
+                if (!empty($smtpEncryption)) {
+                    $mailConfig['mail.mailers.smtp.encryption'] = $smtpEncryption;
+                }
+                if (!empty($smtpFromAddress)) {
+                    $mailConfig['mail.from.address'] = $smtpFromAddress;
+                }
+                if (!empty($smtpFromName)) {
+                    $mailConfig['mail.from.name'] = $smtpFromName;
+                }
+                if (!empty($mailConfig)) {
+                    $mailConfig['mail.default'] = 'smtp';
+                    Config::set($mailConfig);
+                }
+            } catch (\Throwable $e) {
+                report($e);
             }
         }
 
